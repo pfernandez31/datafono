bee515ae9c1c6320fabcc276a2381ad1d7985b30
pfernandez31 <jpblo.3105@gmail.com>
2019-01-18 12:04:58 -0600

    menu

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,23 +1,33 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the MenuPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-menu',
   templateUrl: 'menu.html',
 })
 export class MenuPage {
+  checkout:any = {};
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  
+  print(){
+    let cadena:string = this.checkout.amount; 
+    
+    (<any>window).sunmiInnerPrinter.printerInit()
+        .then(function(any) {
+          (<any>window).sunmiInnerPrinter.printString("Mount " + cadena)
+            .then(function(any){
+              alert("algo " + any);
+            })
+            .catch(function(err){
+              alert("Error " + err);
+            })
+        })
+        .catch(function(err){
+          alert("Error " + err);
+        })
+  }
 
 }
